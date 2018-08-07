41347a35cf643646a60c77382818fc082ff6d503
poiuytsu <jakubjedrzejczyk1993@outlook.com>
2018-08-07 16:02:11 +0200

    Test commit

---
+++ PracaInz/src/test/java/com/example/PracaInz/PracaInzApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.PracaInz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PracaInzApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
